c15f33fae2cd981c30db782a9cf200f5ad818f8e
noave <noavelasco@gmail.com>
2024-02-19 19:57:48 +0100

    240219 3 niveles ingame

--- ingame/player/duck.gd
+++ ingame/player/duck.gd
@@ -1,13 +1,13 @@
 extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var moving_collider = $CollisionShape2D
 
-var move_speed: int = 250
-var push_speed = 125
+var move_speed: int = 150
 
 
-func _ready():
-	position = Vector2(160, 544)
+#func _ready():
+	#position = Vector2(160, 544)
 
 func _physics_process(_delta):
 	#var motion = Vector2()
@@ -15,10 +15,13 @@ func _physics_process(_delta):
 	#motion.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	var motion = Input.get_vector("move_left","move_right", "move_up", "move_down")
 	#print(motion.x)
+
 	velocity = motion * 400
 	
 	update_animation(motion)
 	move_and_slide()
+	if move_and_slide():
+		moving_collider = motion
 
 func update_animation(motion: Vector2):
 	var animation = "idle"

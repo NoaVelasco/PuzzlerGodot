5ed223fe9bb7765931c2be58cc3a26e6d4fa1312
noave <noavelasco@gmail.com>
2024-02-21 20:50:11 +0100

    240221 State Charts y sigo trabado

--- ingame/game/game.gd
+++ ingame/game/game.gd
@@ -3,10 +3,10 @@ extends Node2D
 @onready var player = $Duck
 
 var movements: Array = []
-var commands = {"left": Vector2(-64, 0),
-				"right": Vector2(64, 0),
-				"up": Vector2(0, -64),
-				"down": Vector2(0, 64)}
+var commands = {"left": Vector2(-20, 0),
+				"right": Vector2(20, 0),
+				"up": Vector2(0, -20),
+				"down": Vector2(0, 20)}
 var index_movs = 0
 var len_movs = 0
 
@@ -36,8 +36,12 @@ func _process(_delta):
 # hasta que no quedan movimientos. Entonces se resetea.
 func duckmoves():
 	while index_movs < len_movs:
-		var move: String = movements[index_movs]
-		player.position += commands[move]
+		#var move: String = movements[index_movs]
+		if $Duck.collision != true:
+			player.play_recorded()
+			#print("yay!")
+			#$Duck.position += commands[move]
+			#print(player.position.x)
 		index_movs += 1
 		await get_tree().create_timer(0.5).timeout
 
--- ingame/player/duck.gd
+++ ingame/player/duck.gd
@@ -4,7 +4,9 @@ extends CharacterBody2D
 @onready var moving_collider = $CollisionShape2D
 
 var move_speed: int = 150
-
+var mask_is_centered = true
+var collision = false
+var motion = Vector2(0,0)
 
 #func _ready():
 	#position = Vector2(160, 544)
@@ -13,15 +15,27 @@ func _physics_process(_delta):
 	#var motion = Vector2()
 	#motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	#motion.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	var motion = Input.get_vector("move_left","move_right", "move_up", "move_down")
 	#print(motion.x)
-
+	
+	#var motion = Input.get_vector("move_left","move_right", "move_up", "move_down")
+	
+	if motion:
+		if mask_is_centered == true:
+			moving_collider.position += motion * 5
+			mask_is_centered = false
+	else:
+		moving_collider.position = animated_sprite.position
+		mask_is_centered = true
+	
 	velocity = motion * 400
 	
 	update_animation(motion)
-	move_and_slide()
-	if move_and_slide():
-		moving_collider = motion
+	collision = move_and_slide()
+
+	#if collision != false:
+		#print(collision)
+	#if move_and_slide():
+		#moving_collider = motion
 
 func update_animation(motion: Vector2):
 	var animation = "idle"
@@ -38,9 +52,11 @@ func update_animation(motion: Vector2):
 		animation = "roll_x"
 	elif motion.y > 0:
 		animation = "roll_x"
-#
+		
 	if animated_sprite.animation != animation:
 		animated_sprite.play(animation)
 
-
+func play_recorded():
+	while collision != true:
+		motion += Vector2(10, 0)
 

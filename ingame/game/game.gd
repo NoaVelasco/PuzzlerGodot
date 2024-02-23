076a8de4fc9748fd2dd4b36c09f6d3b7743015a6
noave <noavelasco@gmail.com>
2024-02-23 18:20:48 +0100

    240223 movimiento funciona, toca await

--- ingame/game/game.gd
+++ ingame/game/game.gd
@@ -3,16 +3,13 @@ extends Node2D
 @onready var player = $Duck
 
 var movements: Array = []
-var commands = {"left": Vector2(-20, 0),
-				"right": Vector2(20, 0),
-				"up": Vector2(0, -20),
-				"down": Vector2(0, 20)}
-var index_movs = 0
-var len_movs = 0
+var commands = {"left": Vector2(-1, 0),
+				"right": Vector2(1, 0),
+				"up": Vector2(0, -1),
+				"down": Vector2(0, 1)}
+#var index_movs = 0
+#var len_movs = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -28,23 +25,23 @@ func _process(_delta):
 	elif Input.is_action_just_pressed("print"):
 		print(movements)
 	elif Input.is_action_just_pressed("cadena"):
-		len_movs = len(movements)
+		#len_movs = len(movements)
 		duckmoves()
 
 
-# Cuando el temporizador termina, ejecuta cada movimiento de la lista y se reinicia
-# hasta que no quedan movimientos. Entonces se resetea.
+ #Cuando el temporizador termina, ejecuta cada movimiento de la lista y se reinicia
+ #hasta que no quedan movimientos. Entonces se resetea.
 func duckmoves():
-	while index_movs < len_movs:
+	for move in movements:
+		player.playing = commands[move]
+	#while index_movs < len_movs:
 		#var move: String = movements[index_movs]
-		if $Duck.collision != true:
-			player.play_recorded()
+		#if player.collision != true:
+			#player.play_recorded()
 			#print("yay!")
 			#$Duck.position += commands[move]
 			#print(player.position.x)
-		index_movs += 1
-		await get_tree().create_timer(0.5).timeout
+		#index_movs += 1
+		await get_tree().create_timer(2.5).timeout
 
 	movements = []
-	len_movs = len(movements)
-	index_movs = 0
